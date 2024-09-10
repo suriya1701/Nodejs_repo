72df35dfb434b47e5ba7890eb3cabd3a0099b2a9
suriya1701 <141544638+suriya1701@users.noreply.github.com>
2024-09-10 12:44:22 +0530

    ubuntu install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,13 +3,13 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+sudo nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+DIR="/home/ubuntu/node-app"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+  sudo mkdir ${DIR}
 fi
